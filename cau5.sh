7dc75c60f94c4b90b4467306cd92714d9071232a
tuloi <tuloitrinh@gmail.com>
2024-10-22 09:03:16 +0700

    Add script and files for cau 5 - user2

---
+++ cau5.sh
@@ -0,0 +1,13 @@
+
+
+# Tạo 5 file tên user2_N.txt
+for i in {1..5}; do
+  filename="user2_$i.txt"
+  touch "$filename"
+  
+  # Nếu N lẻ thì thêm nội dung vào file
+  if [ $((i % 2)) -ne 0 ]; then
+    echo "user 2 init" > "$filename"
+  fi
+done
+
